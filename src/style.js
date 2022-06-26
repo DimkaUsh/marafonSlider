722fd72ca58449f7ccc579b4838d5a6a01f513d5
Dmitrii Ushakov <81690819+DimkaUsh@users.noreply.github.com>
2022-06-26 14:59:41 +0800

    feat: add function changeSlide and variable for it

--- src/style.js
+++ src/style.js
@@ -4,6 +4,10 @@ const up = document.querySelector('.up-button');
 const sidebar = document.querySelector('.sidebar');
 const mainSlide = document.querySelector('.main-slide');
 
+mainSlide.style.transform = 'translateY(-300vh)';
+
+let currentSlide = 0;
+
 down.addEventListener('click', () => {
   changeSlide('down');
 });
@@ -11,3 +15,21 @@ down.addEventListener('click', () => {
 up.addEventListener('click', () => {
   changeSlide('up');
 });
+
+function changeSlide(direction) {
+  if (direction === 'down') {
+    currentSlide--;
+    if (currentSlide === -1) {
+      currentSlide = 3;
+    }
+  }
+
+  if (direction === 'up') {
+    currentSlide++;
+    if (currentSlide === 4) {
+      currentSlide = 0;
+    }
+  }
+  mainSlide.style.transform = `translateY(${-300 + currentSlide * 100}vh)`;
+  sidebar.style.transform = `translateY(${currentSlide * -100}vh)`;
+}
