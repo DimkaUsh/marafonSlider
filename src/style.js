4557a5fcc78069227ed795aae1835e6587c59e8c
Dmitrii Ushakov <81690819+DimkaUsh@users.noreply.github.com>
2022-06-26 14:24:23 +0800

    feat: add varibles

---
+++ src/style.js
@@ -0,0 +1,5 @@
+const down = document.querySelector('.down-button');
+const up = document.querySelector('.up-button');
+
+const sidebar = document.querySelector('.sidebar');
+const mainSlide = document.querySelector('.main-slide');
